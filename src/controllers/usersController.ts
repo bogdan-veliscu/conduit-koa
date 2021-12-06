76d01db8f26c2af9b0e272dac260ce12f14206c6
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2021-12-07 00:47:01 +0200

    fixed register and login flows

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,85 +1,125 @@
-import { route, GET, POST, before, inject } from 'awilix-koa';
-import {OK, CREATED, UNAUTHORIZED} from 'http-status-codes';
-import {Context} from 'koa';
-import {Connection} from 'typeorm';
+import { route, GET, POST, PUT, before, inject } from 'awilix-koa';
+import { OK, CREATED, UNAUTHORIZED } from 'http-status-codes';
+import { Context } from 'koa';
+import { Connection } from 'typeorm';
 
 import { assert, object, string } from '@hapi/joi';
 
-import Logger from '../server/Logger'
-import {User} from '../entities/User'
-import UserRepository from "../repositories/UserRepository"
+import Logger from '../server/Logger';
+import { User } from '../entities/User';
+import UserRepository from '../repositories/UserRepository';
 import SecurityService from '../services/SecurityService';
 import AuthenticationMiddleware from '../middleware/AuthenticationMiddleware';
 
 @route('/api')
-export default class UserController{
-    private _userRepository: UserRepository;
-    private _securityService: SecurityService;
+export default class UserController {
+	private _userRepository: UserRepository;
+	private _securityService: SecurityService;
 
-    constructor({connection, securityService}: {connection:Connection; securityService:SecurityService}){
-        this._userRepository = connection.getCustomRepository(UserRepository);
-        this._securityService = securityService;
-    }
+	constructor({ connection, securityService }: { connection: Connection; securityService: SecurityService }) {
+		this._userRepository = connection.getCustomRepository(UserRepository);
+		this._securityService = securityService;
+	}
 
-    @route('/users')
-    @POST()
-    async register(ctx: Context){
-        Logger.info("# users POST:", ctx.request.body)
-        assert(ctx.request.body, object({
-            user: object({
-                username: string().min(5).max(30).required(),
-                email: string().email().required(),
-                password: string().min(5).max(30).required(),
-                bio: string().max(1000),
-                image: string().max(1000),
-            }),
-        }),);
+	@route('/users')
+	@POST()
+	async register(ctx: Context) {
+		Logger.info('# users POST:', ctx.request.body);
+		assert(
+			ctx.request.body,
+			object({
+				user: object({
+					username: string()
+						.min(5)
+						.max(30)
+						.required(),
+					email: string()
+						.email()
+						.required(),
+					password: string()
+						.min(5)
+						.max(30)
+						.required(),
+					bio: string().max(1000),
+					image: string().max(1000),
+				}),
+			}),
+		);
 
-        const user: User = new User();
+		const user: User = new User();
 
-        Object.assign(user, ctx?.request?.body?.user);
-        SecurityService.hashPassword(user);
+		Object.assign(user, ctx?.request?.body?.user);
+		SecurityService.hashPassword(user);
 
-        await this._userRepository.save(user);
-        const token: string = this._securityService.generateToken(user);
+		await this._userRepository.save(user);
+		const token: string = this._securityService.generateToken(user);
 
-        ctx.body = { user:user.toUserJSON(token) };
-        ctx.status = CREATED
-    }
-    
-    @route('/users/login')
-    @POST()
-    async login(ctx: Context) {
-        assert(ctx.request.body,
-            object({
-                user: object({
-                    email: string().email().required(),
-                    password: string().required(),
-                })
-            }))
+		ctx.body = { user: user.toUserJSON(token) };
+		ctx.status = CREATED;
+	}
 
-        const { email, password }: {email:string; password:string} = ctx.request.body.user;
-        const user: User | undefined = await this._userRepository.findOne({email});
+	@route('/users/login')
+	@POST()
+	async login(ctx: Context) {
+		assert(
+			ctx.request.body,
+			object({
+				user: object({
+					email: string()
+						.email()
+						.required(),
+					password: string().required(),
+				}),
+			}),
+		);
 
-        if(!user){
-            ctx.throw(UNAUTHORIZED, 'Unauthorized')
-        }
+		const { email, password }: { email: string; password: string } = ctx.request.body.user;
+		const user: User | undefined = await this._userRepository.findOne({ email });
 
-        if(!SecurityService.verifyHash(password, user.password)){
-            ctx.throw(UNAUTHORIZED, 'Unauthorized')
-        }
+		if (!user) {
+			ctx.throw(UNAUTHORIZED, 'Invalid user');
+		}
 
-        const token: string = this._securityService.generateToken(user);
+		if (!SecurityService.verifyHash(password, user.password)) {
+			ctx.throw(UNAUTHORIZED, 'Invalid password');
+		}
 
-        ctx.body = { user: user.toUserJSON(token) };
-        ctx.status = OK;
-    }
+		const token: string = this._securityService.generateToken(user);
 
-    @route('/user')
-    @GET()
-    @before([inject(AuthenticationMiddleware)])
-    async getCurrentUser(ctx: Context) {
-        ctx.body = { user:ctx.state.user.toUserJSON(ctx.state.token) }
-        ctx.status = OK
-    }
+		ctx.body = { user: user.toUserJSON(token) };
+		ctx.status = OK;
+	}
+
+	@route('/user')
+	@GET()
+	@before([inject(AuthenticationMiddleware)])
+	async getCurrentUser(ctx: Context) {
+		ctx.body = { user: ctx.state.user.toUserJSON(ctx.state.token) };
+		ctx.status = OK;
+	}
+
+	@route('/user')
+	@PUT()
+	@before([inject(AuthenticationMiddleware)])
+	async updateUser(ctx: Context) {
+		assert(
+			ctx.request.body,
+			object({
+				user: object({
+					username: string()
+						.min(5)
+						.max(30),
+					email: string().email(),
+					bio: string().max(1000),
+					image: string().max(1000),
+				}),
+			}),
+		);
+
+		const user: User = ctx.state.user;
+		Object.assign(user, ctx.request.body.user);
+		await this._userRepository.update(user.id, user);
+		ctx.body = { user: user.toUserJSON(ctx.state.token) };
+		ctx.status = OK;
+	}
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,48 +1,47 @@
-import Koa from "koa";
-import {createContainer, AwilixContainer, asValue} from 'awilix';
+import Koa from 'koa';
+import { createContainer, AwilixContainer, asValue } from 'awilix';
 import { scopePerRequest, loadControllers } from 'awilix-koa';
 import cors from '@koa/cors';
-import bodyParser from "koa-bodyparser";
+import bodyParser from 'koa-bodyparser';
 
 import helmet from 'koa-helmet';
 import errorMiddleware from '../middleware/ErrorMiddleware';
-import Logger from './Logger'
-import SecurityService from '../services/SecurityService'
-import { createConnection, Connection } from "typeorm";
+import Logger from './Logger';
+import SecurityService from '../services/SecurityService';
+import { createConnection, Connection } from 'typeorm';
 
 export async function connectWithRetry(): Promise<Connection> {
-    try {
-        console.log("# calling createConnection")
-        return await createConnection();
-        console.log("# after createConnection")
-    } catch (err){
-        Logger.error('failed to connect to db on startup -- retrying in 5 seconds', err);
-        await new Promise((resolve:any) => setTimeout(resolve, 9000));
-        return connectWithRetry();
-    }
+	try {
+		console.log('# calling createConnection');
+		return await createConnection();
+		console.log('# after createConnection');
+	} catch (err) {
+		Logger.error('failed to connect to db on startup -- retrying in 5 seconds', err);
+		await new Promise((resolve: any) => setTimeout(resolve, 500));
+		return connectWithRetry();
+	}
 }
-export async function createApp(): Promise<Koa>{
-    console.log("# createApp")
-    const app = new Koa();
+export async function createApp(): Promise<Koa> {
+	console.log('# createApp');
+	const app = new Koa();
 
-    const securityService: SecurityService = new SecurityService();
-    const connection: Connection = await connectWithRetry();
+	const securityService: SecurityService = new SecurityService();
+	const connection: Connection = await connectWithRetry();
 
-    Logger.info('successfully established DB connection');
+	Logger.info('successfully established DB connection');
 
-    const container: AwilixContainer = createContainer().register({
-        securityService: asValue(securityService),
-        connection: asValue(connection),
-    })
+	const container: AwilixContainer = createContainer().register({
+		securityService: asValue(securityService),
+		connection: asValue(connection),
+	});
 
-    app
-        .use(cors())
-        .use(bodyParser())
-        .use(helmet())
-        .use(scopePerRequest(container))
-        .use(errorMiddleware)
-    .use(loadControllers('../controllers/*.{ts,js}', { cwd:__dirname }));
+	app
+		.use(cors())
+		.use(bodyParser())
+		.use(helmet())
+		.use(scopePerRequest(container))
+		.use(errorMiddleware)
+		.use(loadControllers('../controllers/*.{ts,js}', { cwd: __dirname }));
 
-
-    return app;
+	return app;
 }
